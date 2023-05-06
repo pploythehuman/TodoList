990828e6af17adf74a12b1ea5c035378ea04be32
pploythehuman <kanokpon26@gmail.com>
2023-05-06 16:39:12 +0700

    change onChange to onClick

--- src/components/SortingSelect.tsx
+++ src/components/SortingSelect.tsx
@@ -11,41 +11,42 @@ const SortingSelect:React.FC<SortingSelectProps> = ({ }) => {
     { id: 'select-undone', value: 'undone', label: 'Undone'  },
   ]
 
-  const [selectValue, setSelectValue] = useState('');
+  const [selectValue, setSelectValue] = useState('All');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   }
 
-  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement> | null) => {
+  const handleOptionChange = (event: any) => {
     if (event) {
       setSelectValue(event.target.value);
     }
+    setIsDropdownOpen(false);
   }
 
-  const selectItem = (value: string, id: string, label: string) => (
-    <>
-      <label className="select-item" htmlFor={id}>{label}</label>
-      <input className="option" id={id} type="radio" name="option" value={value} onChange={handleOptionChange} />
-    </>
-  )
+  // const selectItem = (value: string, id: string, label: string) => (
+  //   <>
+  //     <label className="select-item" htmlFor={id}>{label}</label>
+  //     <input className="option" id={id} type="radio" name="option" value={value} onChange={handleOptionChange} />
+  //   </>
+  // )
 
   return (
     <div className="sorting-select-group">
       <button className="sorting-select-button" onClick={toggleDropdown}>
-        <p style={{ fontSize: '13px' }}>Select One</p>
+        <p style={{ fontSize: '13px' }}>{selectValue}</p>
         <div className="arrow" />
       </button>
       <div className={`sorting-select-dropdown ${isDropdownOpen? '' : 'hidden'}`}>
         <label className="select-item" htmlFor="select-all">All</label>
-        <input className="option" id="select-all" type="radio" name="option" value="all" onChange={handleOptionChange} />
+        <input className="option" id="select-all" type="radio" name="option" value="All" onClick={handleOptionChange} />
 
         <label className="select-item" htmlFor="select-done">Done</label>
-        <input className="option" id="select-done" type="radio" name="option" value="done" onChange={handleOptionChange} />
+        <input className="option" id="select-done" type="radio" name="option" value="Done" onClick={handleOptionChange} />
 
         <label className="select-item" htmlFor="select-undone">Undone</label>
-        <input className="option" id="select-undone" type="radio" name="option" value="undone" onChange={handleOptionChange} />
+        <input className="option" id="select-undone" type="radio" name="option" value="Undone" onClick={handleOptionChange} />
       </div>
     </div>
   );
